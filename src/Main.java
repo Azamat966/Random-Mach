6cbf333facd6c3fd496921e3fe932412c6e46fa6
Azamat966 <reallygoooool01@gmail.com>
2023-10-11 17:55:40 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,118 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int sum1 = a%10;
+//                int sum2 = a/10%10;
+//                        int sum3 = a/100%10;
+//                             int sum4 = a/1000%10;
+//
+//        System.out.println(sum3+","+sum2+","+sum1+","+sum4);
+
+
+
+
+
+
+
+//
+//       Random random = new Random();
+//        int a = random.nextInt(1,100);
+//      System.out.println("Резултат первого числа "+a+"="+Math.sqrt(a));
+//       int b = random.nextInt(1,100);
+//        System.out.println("Резултат второго числа "+b+"="+Math.sqrt(b));
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("КАК ТЕБЕ ЗОВУТ:");
+//        String str = sc.nextLine();
+//        System.out.println("СКОЛКО ТЕБЕ ЛЕТ:");
+//        String str1 = sc.nextLine();
+//        System.out.println("В коком городе живещ:");
+//        String str2 = sc.nextLine();
+//        System.out.println("Меня зовут:"+str);
+//        System.out.println("Мой возрасть:"+str1);
+//        System.out.println("Место проживания:"+str2);
+
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        System.out.println(Math.sqrt(a));
+//        int b = scanner.nextInt();
+//        System.out.println(Math.sqrt(b));
+
+
+
+
+//Random random = new Random();
+//int a = random.nextInt(1,50);
+//            System.out.println(a);
+//int b = random.nextInt(1,50);
+//            System.out.println(b);
+//double sum = a+b;
+//        System.out.println(Math.max(a,b));
+//        double xz = sum/2;
+//            System.out.println(xz);
+
+        }
+        }
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        if  (a>b) {
+//            System.out.println(a);
+//        }
+//        else {
+//            System.out.println(b);
+//        }
+
+
+
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.println("Ведите час");
+//        int saat = scanner.nextInt();
+//        System.out.println("Ведите минуту");
+//        int minuta = scanner.nextInt();
+//        System.out.println("Ведите секунду");
+//        int sekunda = scanner.nextInt();
+//
+//        int num1 = saat*3600;
+//        int num2 = minuta*60;
+//        int num3 = sekunda*1;
+//        int kochu = num1+num2+num3;
+//        System.out.println(kochu);
+
+
+//        double a =3.5;
+//        int b = 3;
+//        double protsent = a % b;
+//        System.out.println(protsent);
+
+
+
+//        Scanner scanner = new Scanner(System.in);
+//        String st = scanner.nextLine();
+//        System.out.println("Салам "+st);
+
+
+
+
+
+
+
+
+
